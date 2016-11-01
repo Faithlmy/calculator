09c2441f0523aef78062d53d8db2a2bdabb983b1
lmy <lmengyy@126.com>
2016-11-02 01:00:12 +0800

    lmy

--- calculator/src/Calculator2/Cal.java
+++ calculator/src/Calculator2/Cal.java
@@ -4,7 +4,19 @@ import java.awt.*;
 
 import javax.swing.*;
 
-public class Cal {
+
+
+
+public class Cal extends JFrame {
+	
+	
+	public static void main(String  args[])
+	{
+		Cal cal = new Cal();
+		
+	}
+	
+	
 	
     /** 计算器上的键的显示名字 */  
     private final String[] KEYS = { "7", "8", "9", "/", "sqrt", "4", "5", "6",  
@@ -22,25 +34,15 @@ public class Cal {
     public Cal()
     {
 
-        super();  
-        // 初始化计算器  
-        init();  
-        // 设置计算器的背景颜色  
-        this.setBackground(Color.LIGHT_GRAY);  
-        this.setTitle("计算器");  
-        // 在屏幕(500, 300)坐标处显示计算器  
-        this.setLocation(500, 300);  
-        // 不许修改计算器的大小  
-        this.setResizable(false);  
-        // 使计算器中各组件大小合适  
-        this.pack(); 
-    	
+ 
     }
     /*初始化计算器*/
     public void init()
     {
 
-    	resultText.setHorizontalAlignment(JTextField.RIGHT);
+    	JPanel calckeysPanel = new JPanel(); 
+    	calckeysPanel.setLayout(new GridLayout(4, 5, 3, 3));
+
     }
 
 
---
+++ calculator/src/lianxi/lx.java
@@ -0,0 +1,45 @@
+package lianxi;
+
+
+import java.awt.*;
+import javax.swing.*;
+
+public class lx extends JFrame{
+	
+	//定义组件
+	JButton jb1, jb2, jb3, jb4, jb5;
+	//构造函数
+	public lx()
+	{
+		//创建组件
+		jb1 = new JButton("中间");
+		jb2 = new JButton("东");
+		jb3 = new JButton("西");
+		jb4 = new JButton("南");
+		jb5 = new JButton("北");
+		
+		//添加组件
+		this.add(jb1, BorderLayout.CENTER);
+		this.add(jb2, BorderLayout.EAST);
+		this.add(jb3, BorderLayout.WEST);
+		this.add(jb4, BorderLayout.SOUTH);
+		this.add(jb5, BorderLayout.NORTH);
+		
+		
+		//设置窗体属性
+		this.setTitle("布局");
+		this.setSize(300, 400);
+		this.setLocation(200, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//显示窗体
+		this.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		lx lx1 = new lx();
+	}
+
+}
