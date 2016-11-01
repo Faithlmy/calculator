c44f9e84437508089e2859206356c67e4712c365
lmy <lmengyy@126.com>
2016-11-02 01:43:58 +0800

    分支

--- calculator/src/Calculator2/Cal.java
+++ calculator/src/Calculator2/Cal.java
@@ -54,7 +54,7 @@ public class Cal extends JFrame {
         } 
     	
     	this.setTitle("¼ÆËãÆ÷");
-    	this.setSize(400, 400);
+    	this.setSize(500, 500);
 		this.setLocation(200, 200);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
