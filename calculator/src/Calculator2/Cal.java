243fc17ff03c559b313e224cbb3169103ee4418b
lmy <lmengyy@126.com>
2016-11-03 23:58:34 +0800

    cc

--- calculator/src/Calculator2/Cal.java
+++ calculator/src/Calculator2/Cal.java
@@ -114,10 +114,68 @@ public class Cal extends JFrame implements ActionListener{
 
     }
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	 public void actionPerformed(ActionEvent e) {  
+        // 获取事件源的标签  
+        String label = e.getActionCommand();  
+        if (label.equals(COMMAND[0])) {  
+            // 用户按了"Backspace"键  
+            handleBackspace();  
+        } else if (label.equals(COMMAND[1])) {  
+            // 用户按了"CE"键  
+            resultText.setText("0");  
+        } else if (label.equals(COMMAND[2])) {  
+            // 用户按了"C"键  
+            handleC();  
+        } else if ("0123456789.".indexOf(label) >= 0) {  
+            // 用户按了数字键或者小数点键  
+            handleNumber(label);  
+            // handlezero(zero);  
+        } else {  
+            // 用户按了运算符键  
+            handleOperator(label);  
+        }  
+    }
+	private void handleNumber(String label) {
 		// TODO Auto-generated method stub
+		 if (firstDigit) {  
+	            // 输入的第一个数字  
+	            resultText.setText(key);  
+	        } else if ((key.equals(".")) && (resultText.getText().indexOf(".") < 0)) {  
+	            // 输入的是小数点，并且之前没有小数点，则将小数点附在结果文本框的后面  
+	            resultText.setText(resultText.getText() + ".");  
+	        } else if (!key.equals(".")) {  
+	            // 如果输入的不是小数点，则将数字附在结果文本框的后面  
+	            resultText.setText(resultText.getText() + key);  
+	        }  
+	        // 以后输入的肯定不是第一个数字了  
+	        firstDigit = false;  
+	}
+	private void handleC() {
+		// TODO Auto-generated method stub
+		resultText.setText("0");  
+        firstDigit = true;  
+        operator = "=";  
 		
 	}
+	private void handleBackspace() {
+		// TODO Auto-generated method stub
+		 String text = resultText.getText();  
+	        int i = text.length();  
+	        if (i > 0) {  
+	            // 退格，将文本最后一个字符去掉  
+	            text = text.substring(0, i - 1);  
+	            if (text.length() == 0) {  
+	                // 如果文本没有了内容，则初始化计算器的各种值  
+	                resultText.setText("0");  
+	                firstDigit = true;  
+	                operator = "=";  
+	            } else {  
+	                // 显示新的文本  
+	                resultText.setText(text);  
+	            }  
+	        }  
+		
+	}  
 
 
 
